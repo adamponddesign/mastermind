06d28da4fa668f4e15cbca8883fd39414d905415
Adam Pond <adam.pond@hotmail.co.uk>
2019-03-29 14:46:54 +0000

    game logic working, results returned as strings in an array

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  //  ** create variables
-  //  - make an empty array for userGuess & computerRandomCode
+  //  ** variables
   let userGuess = []
-  let userGuessOne = document.querySelector('#userGuessOne')
-  let userGuessTwo = document.querySelector('#userGuessTwo')
-  let userGuessThree = document.querySelector('#userGuessThree')
-  let computerRandomCode = []
+  const computerRandomCode = []
   let counter = 0
+  const userGuessOne = document.querySelector('#userGuessOne')
+  const userGuessTwo = document.querySelector('#userGuessTwo')
+  const userGuessThree = document.querySelector('#userGuessThree')
   const form = document.querySelector('form')
-  let input = document.querySelector('input')
 
+
+
+  // let resultsArray = []
+  // let allresultsSquaresDom = document.querySelectorAll('.resultsContainer > div')
+  // let whiteCounter = 0
+  // let redCounter = 0
+  // let input = document.querySelector('input')
+  // let resultAreaOneDOM = document.querySelector('#resultAreaOne')
+  // let resultAreaTwoDOM = document.querySelector('#resultAreaTwo')
+  // let resultAreaThreeDOM = document.querySelector('#resultAreaThree')
 
 
 
   function getComputerRandomCodeFunction() {              // computer random Three digit number ***********
-
     while(counter < 3 ){                                  // while counter is less than three...
       const number = Math.floor(Math.random() * 3) + 1    //create a randow number between 1 and 3
       computerRandomCode.push(number)                     //  add the number to the computerRandomCode array..
@@ -26,11 +33,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
   getComputerRandomCodeFunction()                         // call getComputerRandomCodeFunction
-  // ********************************************
+  // *******************************************
 
 
 
-  // user input and result
+
+
+  // user input and game play
 
   form.addEventListener('submit', (e) => {                //add a 'submit' event listener to the userGuess form
     e.preventDefault()
@@ -38,6 +47,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     console.log('user guess ' + userGuess)                  // log userGuess.. which is now an array
 
+    const colorResult = userGuess.map((element, index) => {
+
+      if (element === computerRandomCode[index]) {
+        return 'red'
+      } else if (computerRandomCode.includes(element)) {
+        return 'white'
+      } else
+        return 'Null'
+    })
+
+    console.log(colorResult)
 
   })                                                      // event listener closing tag
 
@@ -47,81 +67,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-  /*
-NOTES
-
-
-** create DOM target areas
-
-
-
-** create UserGuess code
-
-    - get input from user
-        - user guess 1  (choose a number between 1 and 3)   add to userGuess array
-        - user guess 2  (choose a number between 1 and 3)   add to userGuess array
-        - user guess 3  (choose a number between 1 and 3)   add to userGuess array
-
-    - UserGuess should contain a 3 number array
-    - console.log(userGuess)
-
-** Game Logic
-
-- compare the two arrays
-
-    position one
-
-  - if (userGuess[0] === computerRandomCode[0]) {                                                    // perfect match... position and number
-  add class of red to one of the result area divs                                                    // add a red circle
-  } else if (userGuess[0] === computerRandomCode[1] && userGuess[0] === computerRandomCode[2]) {     // match number but not position of both other options
-  add class of white to TWO of the result area divs                                                  // add two whites
-} else if (userGuess[0] === computerRandomCode[1] || userGuess[0] === computerRandomCode[2]) {       // match number but not position of one other options
-    add class of white to ONE of the result area divs                                                // add one white
-  } else {                                                                                           // no matches
-    do nothing                                                                                       // do nothing
-  }
-
-    position two
-
-    - if (userGuess[1] === computerRandomCode[1]) {                                                    // perfect match... position and number
-    add class of red to one of the result area divs                                                    // add a red circle
-  } else if (userGuess[1] === computerRandomCode[0] && userGuess[1] === computerRandomCode[2]) {     // match number but not position of both other options
-    add class of white to TWO of the result area divs                                                  // add two whites
-  } else if (userGuess[1] === computerRandomCode[0] || userGuess[1] === computerRandomCode[2]) {       // match number but not position of one other options
-      add class of white to ONE of the result area divs                                                // add one white
-    } else {                                                                                           // no matches
-      do nothing                                                                                       // do nothing
-    }
-
-    position three
-
-    - if (userGuess[2] === computerRandomCode[2]) {                                                    // perfect match... position and number
-    add class of red to one of the result area divs                                                    // add a red circle
-  } else if (userGuess[2] === computerRandomCode[0] && userGuess[2] === computerRandomCode[1]) {     // match number but not position of both other options
-    add class of white to TWO of the result area divs                                                  // add two whites
-  } else if (userGuess[2] === computerRandomCode[0] || userGuess[2] === computerRandomCode[1]) {       // match number but not position of one other options
-      add class of white to ONE of the result area divs                                                // add one white
-    } else {                                                                                           // no matches
-      do nothing                                                                                       // do nothing
-    }
-
-
-
-
-
-*/
 
 
 
