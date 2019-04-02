12056d9f52d5d11e69e85fb1451adf2a4406d5d3
Adam Pond <adam.pond@hotmail.co.uk>
2019-04-02 11:34:45 +0100

    submit button working on different lines

--- app.js
+++ app.js
@@ -13,12 +13,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const userSelectButtonsDOM = document.querySelectorAll('.userSelectButtons div')
 
 
-  const submitGuess = document.querySelector('.submitGuess')
+  const allSubmitBoxes = document.querySelectorAll('.submitGuess')  // all divs with the class of .submitGuess
+  // let submitIndex = 0
+  // const
+
+
   const resultAreaOneDOM = document.querySelector('#resultAreaOne')
   const resultAreaTwoDOM = document.querySelector('#resultAreaTwo')
   const resultAreaThreeDOM = document.querySelector('#resultAreaThree')
   const resultAreaFourDOM = document.querySelector('#resultAreaFour')
   const winner = document.querySelector('#winner')
+
 
 
 
@@ -31,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
   currentRow.classList.add('activeRow')                // to that 'currentRow' add a class of active
 
 
-
+  // listener on every space... only to work if the containing parent has the class of activeRow
   allSpaces.forEach(element => {                      // for each space div within the 'allspaces' array
     element.addEventListener('click', (e) => {        //  add a click eventlistener
       if (!e.target.parentNode.classList.contains('activeRow')) {      // if the parent of the element clicked DOESN'T have the class of active..
@@ -98,91 +103,104 @@ document.addEventListener('DOMContentLoaded', () => {
   //        add all class names to userGuess array
   //        determine color result
   //        shuffle the color result
-  submitGuess.addEventListener('click', () => {
-    userGuess = [userGuessOne.className,userGuessTwo.className,userGuessThree.className,userGuessFour.className]   // add all class names to userGuess array
-    console.log('the user guess is ' + userGuess)
 
 
-    //       determine color result
-    const colorResult = userGuess.map((element, i) => {     // .map move over each element and index in the userGuess array
-      if (element === computerRandomCode[i]) {              // if current element equals computerRandomCode, (current index)
-        return 'green'
-      } else if (computerRandomCode.includes(element)) {        // if computerRandomCode includes the current element in any location
-        return 'orange'
-      } else
-        return ''
-    })
-    console.log('the result is ' + colorResult)
-    //       shuffle the color result
-    const shuffledResult = shuffle(colorResult)
-    console.log('the shuffled result is ' + shuffle(colorResult))
+  allSubmitBoxes.forEach(element => {
 
-    resultAreaOneDOM.innerText = shuffledResult[0]
-    resultAreaTwoDOM.innerText = shuffledResult[1]
-    resultAreaThreeDOM.innerText = shuffledResult[2]
-    resultAreaFourDOM.innerText = shuffledResult[3]
-
-    if (resultAreaOneDOM === 'green' && resultAreaTwoDOM === 'green' && resultAreaThreeDOM === 'green' && resultAreaFourDOM === 'green') {
-      winner.className = ''
-    }
-
-    // ***************    change color via class list change based on inner text
-    // Result box one
-    if (resultAreaOneDOM.innerText === 'green') {
-      resultAreaOneDOM.classList.add('green')
-      resultAreaOneDOM.innerText = ''
-    } else if (resultAreaOneDOM.innerText === 'orange') {
-      resultAreaOneDOM.classList.add('orange')
-      resultAreaOneDOM.innerText = ''
-    }
-
-    // Result box two
-    if (resultAreaTwoDOM.innerText === 'green') {
-      resultAreaTwoDOM.classList.add('green')
-      resultAreaTwoDOM.innerText = ''
-    } else if (resultAreaTwoDOM.innerText === 'orange') {
-      resultAreaTwoDOM.classList.add('orange')
-      resultAreaTwoDOM.innerText = ''
-    }
-
-    // Result box three
-    if (resultAreaThreeDOM.innerText === 'green') {
-      resultAreaThreeDOM.classList.add('green')
-      resultAreaThreeDOM.innerText = ''
-    } else if (resultAreaThreeDOM.innerText === 'orange') {
-      resultAreaThreeDOM.classList.add('orange')
-      resultAreaThreeDOM.innerText = ''
-    }
-
-    // Result box four
-    if (resultAreaFourDOM.innerText === 'green') {
-      resultAreaFourDOM.classList.add('green')
-      resultAreaFourDOM.innerText = ''
-    } else if (resultAreaFourDOM.innerText === 'orange') {
-      resultAreaFourDOM.classList.add('orange')
-      resultAreaFourDOM.innerText = ''
-    }
+    element.addEventListener('click', (e) => {
+      if (!e.target.parentNode.classList.contains('activeRow')) {      // if the parent of the element clicked DOESN'T have the class of active..
+        return false                                                // do nothing
+      } else {
+        userGuess = [userGuessOne.className,userGuessTwo.className,userGuessThree.className,userGuessFour.className]   // add all class names to userGuess array
+        console.log('the user guess is ' + userGuess)
 
 
+        //       determine color result
+        const colorResult = userGuess.map((element, i) => {     // .map move over each element and index in the userGuess array
+          if (element === computerRandomCode[i]) {              // if current element equals computerRandomCode, (current index)
+            return 'green'
+          } else if (computerRandomCode.includes(element)) {        // if computerRandomCode includes the current element in any location
+            return 'orange'
+          } else
+            return ''
+        })
+        console.log('the result is ' + colorResult)
+        //       shuffle the color result
+        const shuffledResult = shuffle(colorResult)
+        console.log('the shuffled result is ' + shuffle(colorResult))
 
-    // winner box when 4 reds
-    if (resultAreaOneDOM.classList.contains('green') &&
+        resultAreaOneDOM.innerText = shuffledResult[0]
+        resultAreaTwoDOM.innerText = shuffledResult[1]
+        resultAreaThreeDOM.innerText = shuffledResult[2]
+        resultAreaFourDOM.innerText = shuffledResult[3]
+
+        if (resultAreaOneDOM === 'green' && resultAreaTwoDOM === 'green' && resultAreaThreeDOM === 'green' && resultAreaFourDOM === 'green') {
+          winner.className = ''
+        }
+
+        // ***************    change color via class list change based on inner text
+        // Result box one
+        if (resultAreaOneDOM.innerText === 'green') {
+          resultAreaOneDOM.classList.add('green')
+          resultAreaOneDOM.innerText = ''
+        } else if (resultAreaOneDOM.innerText === 'orange') {
+          resultAreaOneDOM.classList.add('orange')
+          resultAreaOneDOM.innerText = ''
+        }
+
+        // Result box two
+        if (resultAreaTwoDOM.innerText === 'green') {
+          resultAreaTwoDOM.classList.add('green')
+          resultAreaTwoDOM.innerText = ''
+        } else if (resultAreaTwoDOM.innerText === 'orange') {
+          resultAreaTwoDOM.classList.add('orange')
+          resultAreaTwoDOM.innerText = ''
+        }
+
+        // Result box three
+        if (resultAreaThreeDOM.innerText === 'green') {
+          resultAreaThreeDOM.classList.add('green')
+          resultAreaThreeDOM.innerText = ''
+        } else if (resultAreaThreeDOM.innerText === 'orange') {
+          resultAreaThreeDOM.classList.add('orange')
+          resultAreaThreeDOM.innerText = ''
+        }
+
+        // Result box four
+        if (resultAreaFourDOM.innerText === 'green') {
+          resultAreaFourDOM.classList.add('green')
+          resultAreaFourDOM.innerText = ''
+        } else if (resultAreaFourDOM.innerText === 'orange') {
+          resultAreaFourDOM.classList.add('orange')
+          resultAreaFourDOM.innerText = ''
+        }
+
+
+        // winner box when 4 reds
+        if (resultAreaOneDOM.classList.contains('green') &&
         resultAreaTwoDOM.classList.contains('green') &&
         resultAreaThreeDOM.classList.contains('green') &&
         resultAreaFourDOM.classList.contains('green')
-    ) {
-      winner.classList.remove('hidden')
-    } else {
-      currentRow.classList.remove('activeRow')
-      rowIndex++
-      currentRow = rows[rowIndex]
-      currentRow.classList.add('activeRow')
-    }
+        ) {
+          winner.classList.remove('hidden')
+        } else {
+          currentRow.classList.remove('activeRow')
+          rowIndex++
+          currentRow = rows[rowIndex]
+          currentRow.classList.add('activeRow')
+        }
 
-
-
-
+      }
+    })
   })
+
+
+
+
+
+
+
+
 
 
 
